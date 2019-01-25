cac6673363637b6cec12f99cea9f74733b0f2654
mcharman <mcharmanguy@gmail.com>
2019-01-25 11:39:47 -0700

    Add files via upload

---
+++ angular/course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css']
+})
+export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('quantityInput') quantityInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingQty = this.quantityInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingQty);
+    this.ingredientAdded.emit(newIngredient);
+  }
+
+}
